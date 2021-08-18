4b8191af82959a168278db3f48a1c614bcc69b54
Mehedi390 <mh0816150@gmail.com>
2021-08-17 23:04:38 -0700

    finished

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function getInputValue(inputValue, priceId, price, add) {
+function productPrice(inputValue, priceId, price, add) {
     const inputFiled = document.getElementById(inputValue);
     let inputNumber = inputFiled.value;
     if (add == true) {
@@ -10,19 +10,41 @@ function getInputValue(inputValue, priceId, price, add) {
     inputFiled.value = inputNumber;
     const iphnPrice = document.getElementById(priceId);
     iphnPrice.innerText = inputNumber * price;
+    CalulateSubToal()
+}
+function InputProdcutNumber(inputIdName) {
+    const inpuTvalue = document.getElementById(inputIdName);
+    return inpuTvalue.value;
 
+}
+function CalulateSubToal() {
+    const phnTotal = InputProdcutNumber('iphn-input-value') * 1219;
+    const caseTotal = InputProdcutNumber('case-input-value') * 59;
+    // calculate sub total 
+    let getSubTotal = document.getElementById('sub-total');
+    const subTotal = phnTotal + caseTotal;
+    getSubTotal.innerText = parseInt(subTotal);
+    // calculate tax amount 
+    const getTaxAmount = document.getElementById('tax-amoun');
+    const tax = subTotal * 0.07;
+    getTaxAmount.innerText = tax;
+    // calculate total amount 
+    const totalAmount = document.getElementById('total-amount');
+    totalAmount.innerText = tax + subTotal;
 }
 // for iphon 
 document.getElementById('palus-iph-btn').addEventListener('click', function () {
-    const inputFildIphn = getInputValue('input-value', 'iph-price', '1219', true);
+    const inputFildIphn = productPrice('iphn-input-value', 'iph-price', '1219', true);
+
+
 })
 document.getElementById('iphon-minus-btn').addEventListener('click', function () {
-    const inputFildIphn = getInputValue('input-value', 'iph-price', '1219', false);
+    const inputFildIphn = productPrice('iphn-input-value', 'iph-price', '1219', false);
 })
 // for case 
 document.getElementById('case-plus-btn').addEventListener('click', function () {
-    const caseInput = getInputValue('case-input-value', 'case-price', '59', true);
+    const caseInput = productPrice('case-input-value', 'case-price', '59', true);
 })
 document.getElementById('case-minus-btn').addEventListener('click', function () {
-    const caseInput = getInputValue('case-input-value', 'case-price', '59', false);
+    const caseInput = productPrice('case-input-value', 'case-price', '59', false);
 })
